dac9a76cc88d36fc5bb2cdf8cdb65bbfffdfb406
GabrielGruit <ggabri432@gmail.com>
2023-06-28 11:05:17 +0200

    ajout du noeud

--- src/models/FormuleEliminationDirecte.java
+++ src/models/FormuleEliminationDirecte.java
@@ -1,13 +1,33 @@
 package models;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class FormuleEliminationDirecte implements FormuleChampionnat {
-    private Match finalMatch; // hypothétiquement, une structure de match qui représente la finale
+    private Noeud finale;// hypothétiquement, une structure de match qui représente la finale
 
     // Méthode pour organiser des matchs
     public void organiserMatches(List<Equipe> equipes) {
-        // Implémentation dépendra de la logique spécifique de l'organisation des matchs d'élimination directe
+        
+        Queue<Noeud> queue = new LinkedList<>();
+        for (int i = 0; i < equipes.size(); i += 2) {
+            Match match = new Match(equipes.get(i), equipes.get(i + 1));
+            queue.add(new Noeud(match));
+        }
+
+        while (queue.size() > 1) {
+            Noeud noeud1 = queue.poll();
+            Noeud noeud2 = queue.poll();
+            // Un match "virtuel" qui sera mis à jour avec les bonnes équipes plus tard
+            Match match = new Match(null, null);
+            Noeud parent = new Noeud(match);
+            parent.enfantGauche = noeud1;
+            parent.enfantDroite = noeud2;
+            queue.add(parent);
+        }
+
+        finale = queue.poll();
     }
 
     // Méthode pour générer un classement
---
+++ src/models/Noeud.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Noeud {
+    Match match;
+    Noeud enfantGauche;
+    Noeud enfantDroite;
+
+    public Noeud(Match match) {
+        this.match = match;
+    }
+
+    // getters and setters
+    
+}
